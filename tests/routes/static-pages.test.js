3b6e27a9dbfbc9d8dbe61927eeb9459e853326dd
G.Ambatte <sean@robertson-family.nz>
2022-03-23 10:54:37 +1300

    Insert 10s delay in testing steps

--- tests/routes/static-pages.test.js
+++ tests/routes/static-pages.test.js
@@ -1,14 +1,21 @@
+const { before } = require('lodash');
 const supertest = require('supertest');
 
-const startTime = performance.now();
+const startTime = Date.now();
 
 // Mimic https responses to avoid being redirected all the time
 const app = supertest.agent(require('app.js').app)
     .set('X-Forwarded-Proto', 'https');
 
-const endTime = performance.now();
+const endTime = Date.now();
 
 console.debug(`Initialization of the app and supertest took ${endTime - startTime} milliseconds`);
+
+before('some test title', async ()=>{
+	const foo = true;
+	await new Promise((r)=>{setTimeout(r, 10000);});
+	expect(foo).toBeDefined();
+});
 
 describe('Tests for static pages', ()=>{
 	it('Home page works', ()=>{
